5f365856c8786ac080aa6b5d13c87cba45098f13
JoaoNeto4 <joaoquatlo@hotmail.com>
2023-11-09 22:21:21 -0300

    add models in admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Recipe
 
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
+    ...
+admin.site.register(Category)
 
 
-    admin.site.register(Category)
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+    
