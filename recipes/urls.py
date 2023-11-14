fd472d3f07ec24b0235944e0c3be632c693a29c0
JoaoNeto4 <joaoquatlo@hotmail.com>
2023-11-13 22:59:55 -0300

    django shell and real model

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,7 +5,9 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('recipes/category/<int:category_id>/', views.category, name="category"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
+    
     #path('sobre/', sobre),
     #path('contato/', contato),
 ]
