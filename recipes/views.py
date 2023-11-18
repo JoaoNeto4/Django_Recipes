23808c7fc965c6189b1e40c56ebfa10c0a03b28e
JoaoNeto4 <joaoquatlo@hotmail.com>
2023-11-18 00:02:40 -0300

    User shell and 404

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from django.http import Http404
 
 from recipes.models import Recipe
 
@@ -13,15 +14,25 @@ def home(request):
     })
 """
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    #recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True
+        ).order_by('-id')
+    
+    if not recipes:
+        raise Http404('Not found')
+
+
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category |',
     })
 
 def recipe(request, id):
