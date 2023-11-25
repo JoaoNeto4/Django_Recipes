f314bf96e383126a9019fa0b5ab3030868ca8778
JoaoNeto4 <joaoquatlo@hotmail.com>
2023-11-24 21:21:52 -0300

    linebreaksbr and init tests

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class RecipeURLsTests(TestCase):
+    
+    def test_the_pytests_is_ok(self):
+        assert 1 == 1, 'um é igual a um'
